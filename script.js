5ec91ee7f906e03dd54795ef894edca0077ec787
Alexander Oshkov <sasha.oshkov03@gmail.com>
2023-05-12 15:56:16 +0300

    added comments to JS

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 let display = document.getElementById('screen');
 
+// Очистка поля ввода
 const wipe = () => {
     display.value = '';
     display.style="font-size: 90px";
 }
 
+// Вывод цифр на экран
 const show = (n) => {
     display.value += n;
 
+    // Проверка на количество цифр в строке, чтобы все умещались
     if ((display.value).length === 7) {
-        display.style="font-size: 70px";
+        display.style="font-size: 70px"; // Уменьшение шрифта цифр
     } else if ((display.value).length === 9) {
         display.style="font-size: 60px";
     } else if ((display.value).length === 10) {
@@ -22,15 +25,15 @@ const show = (n) => {
     }
 }
 
+// Подсчет результата
 const calc = () => {
-    console.log((display.value).replace(/%/g, "/100"))
+    console.log((display.value).replace(/%/g, "/100")) // Замена символа процента на "/100"
 
+    display.value = eval((display.value).replace(/%/g, "/100")); // Подсчет результата
 
-    // display.value = eval(display.value);
-    display.value = eval((display.value).replace(/%/g, "/100"));
-
+    // Проверка на количество цифр в строке, чтобы все умещались
     if ((display.value).length === 7) {
-        display.style="font-size: 70px";
+        display.style="font-size: 70px"; // Уменьшение шрифта цифр
     } else if ((display.value).length === 9) {
         display.style="font-size: 60px";
     } else if ((display.value).length === 10) {
